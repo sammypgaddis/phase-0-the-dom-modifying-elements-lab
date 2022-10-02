fb0bd9c25a715c58f13f977ae31002b01a07d299
Samuel Gaddis <siddag14@live.com>
2022-10-02 18:47:31 -0400

    commit tree 04eb121081d704a73e8f2c0661b907f07d622ac1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here! WRITING HTML IN JAVASCRIPT
+const element = document.getElementById('main');
+element.remove('main');
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Sam is the champion";
+
